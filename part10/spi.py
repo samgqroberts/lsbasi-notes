f7740a5807af724d586db89ce561e9722a9a62be
Sam Roberts <samgqroberts@gmail.com>
2021-03-15 10:56:29 -0400

    part10 exercise wip - lexer can handle full sample program

--- part10/spi.py
+++ part10/spi.py
@@ -3,13 +3,29 @@ If you haven't done so yet, then, as an exercise, re-implement the interpreter i
 without looking at the source code and use part10.pas as your test input file.
 """
 
-EOF = 'EOF'
+# Token Types
+PROGRAM = 'PROGRAM'
+SEMI = 'SEMI'
+DOT = 'DOT'
+VAR = 'VAR'
+ID = 'ID'
+COMMA = 'COMMA'
+COLON = 'COLON'
 INTEGER = 'INTEGER'
 REAL = 'REAL'
+BEGIN = 'BEGIN'
+END = 'END'
+ASSIGN = 'ASSIGN'
 PLUS = 'PLUS'
 MINUS = 'MINUS'
+MUL = 'MUL'
+INTEGER_DIV = 'INTEGER_DIV'
+FLOAT_DIV = 'FLOAT_DIV'
+INTEGER_CONST = 'INTEGER_CONST'
+REAL_CONST = 'REAL_CONST'
 LPAREN = 'LPAREN'
 RPAREN = 'RPAREN'
+EOF = 'EOF'
 
 
 class Token(object):
@@ -37,12 +53,29 @@ class Lexer(object):
             return None
         return self.text[self.pos]
 
+    def peek(self):
+        next_pos = self.pos + 1
+        if next_pos >= len(self.text):
+            return None
+        return self.text[next_pos]
+
+    def error(self):
+        raise Exception(
+            'An error occurred while lexing the text. Current character: ' + self.current_char())
+
     def advance(self):
         self.pos += 1
 
     def skip_whitespace(self):
         while self.current_char() is not None and self.current_char().isspace():
             self.advance()
+
+    def skip_comment(self):
+        while self.current_char() != '}':
+            if (self.current_char() is None):
+                self.error()
+            self.advance()
+        self.advance()
 
     def number(self):
         value = ''
@@ -55,15 +88,37 @@ class Lexer(object):
             while (self.current_char() is not None and self.current_char().isdigit()):
                 value += self.current_char()
                 self.advance()
-            return Token(REAL, float(value))
+            return Token(REAL_CONST, float(value))
         else:
-            return Token(INTEGER, int(value))
+            return Token(INTEGER_CONST, int(value))
+
+    def alpha(self):
+        value = ''
+        while self.current_char() is not None and (self.current_char().isalpha() or self.current_char().isdigit()):
+            value += self.current_char()
+            self.advance()
+        if (value.upper() == 'PROGRAM'):
+            return Token(PROGRAM, value)
+        if (value.upper() == 'VAR'):
+            return Token(VAR, value)
+        if (value.upper() == 'BEGIN'):
+            return Token(BEGIN, value)
+        if (value.upper() == 'END'):
+            return Token(END, value)
+        if (value.upper() == 'DIV'):
+            return Token(INTEGER_DIV, value)
+        if (value.upper() == 'REAL'):
+            return Token(REAL, value)
+        if (value.upper() == 'INTEGER'):
+            return Token(INTEGER, value)
+        return Token(ID, value)
 
     def get_next_token(self):
-        if (self.current_char() is None):
-            return Token(EOF, '')
-        if (self.current_char().isspace()):
+        if (self.current_char() is not None and self.current_char().isspace()):
             self.skip_whitespace()
+        if (self.current_char() == '{'):
+            self.skip_comment()
+            return self.get_next_token()
         if (self.current_char() is None):
             return Token(EOF, '')
         if (self.current_char().isdigit()):
@@ -76,6 +131,14 @@ class Lexer(object):
             token = Token(MINUS, '-')
             self.advance()
             return token
+        if (self.current_char() == '*'):
+            token = Token(MUL, '*')
+            self.advance()
+            return token
+        if (self.current_char() == '/'):
+            token = Token(FLOAT_DIV, '/')
+            self.advance()
+            return token
         if (self.current_char() == '('):
             token = Token(LPAREN, '(')
             self.advance()
@@ -84,6 +147,31 @@ class Lexer(object):
             token = Token(RPAREN, ')')
             self.advance()
             return token
+        if (self.current_char() == ';'):
+            token = Token(SEMI, ';')
+            self.advance()
+            return token
+        if (self.current_char() == ','):
+            token = Token(COMMA, ',')
+            self.advance()
+            return token
+        if (self.current_char() == ':'):
+            next_char = self.peek()
+            if (next_char is not None and next_char == '='):
+                token = Token(ASSIGN, ':=')
+                self.advance()
+                self.advance()
+                return token
+            token = Token(COLON, ':')
+            self.advance()
+            return token
+        if (self.current_char() == '.'):
+            token = Token(DOT, '.')
+            self.advance()
+            return token
+        if (self.current_char().isalpha()):
+            return self.alpha()  # method advances sufficiently
+        self.error()
 
 
 def lex(text):
--- part10/spitests.py
+++ part10/spitests.py
@@ -1,6 +1,8 @@
+from spi import INTEGER_DIV
 import unittest
 
-from spi import lex, Token, INTEGER, PLUS, MINUS, LPAREN, REAL, RPAREN  # , parse, evaluate
+# , parse, evaluate
+from spi import lex, Token, INTEGER_CONST, PLUS, MINUS, PROGRAM, LPAREN, REAL_CONST, RPAREN, ID, SEMI, VAR, COLON, INTEGER, COMMA, REAL, BEGIN, ASSIGN, MUL, END, FLOAT_DIV, DOT
 
 sample_program = open("part10.pas", "r").read()
 
@@ -10,14 +12,86 @@ class TestSuite(unittest.TestCase):
         text = " 1 + 2 -   (-3.4) "
         tokens = lex(text)
         expected = [
-            Token(INTEGER, 1),
+            Token(INTEGER_CONST, 1),
             Token(PLUS, '+'),
-            Token(INTEGER, 2),
+            Token(INTEGER_CONST, 2),
             Token(MINUS, '-'),
             Token(LPAREN, '('),
             Token(MINUS, '-'),
-            Token(REAL, 3.4),
+            Token(REAL_CONST, 3.4),
             Token(RPAREN, ')')
+        ]
+        self.assertEqual(tokens, expected)
+
+    def test_lexer_program(self):
+        tokens = lex(sample_program)
+        expected = [
+            Token(PROGRAM, 'PROGRAM'),
+            Token(ID, 'Part10'),
+            Token(SEMI, ';'),
+            Token(VAR, 'VAR'),
+            Token(ID, 'number'),
+            Token(COLON, ':'),
+            Token(INTEGER, 'INTEGER'),
+            Token(SEMI, ';'),
+            Token(ID, 'a'),
+            Token(COMMA, ','),
+            Token(ID, 'b'),
+            Token(COMMA, ','),
+            Token(ID, 'c'),
+            Token(COMMA, ','),
+            Token(ID, 'x'),
+            Token(COLON, ':'),
+            Token(INTEGER, 'INTEGER'),
+            Token(SEMI, ';'),
+            Token(ID, 'y'),
+            Token(COLON, ':'),
+            Token(REAL, 'REAL'),
+            Token(SEMI, ';'),
+            Token(BEGIN, 'BEGIN'),
+            Token(BEGIN, 'BEGIN'),
+            Token(ID, 'number'),
+            Token(ASSIGN, ':='),
+            Token(INTEGER_CONST, 2),
+            Token(SEMI, ';'),
+            Token(ID, 'a'),
+            Token(ASSIGN, ':='),
+            Token(ID, 'number'),
+            Token(SEMI, ';'),
+            Token(ID, 'b'),
+            Token(ASSIGN, ':='),
+            Token(INTEGER_CONST, 10),
+            Token(MUL, '*'),
+            Token(ID, 'a'),
+            Token(PLUS, '+'),
+            Token(INTEGER_CONST, 10),
+            Token(MUL, '*'),
+            Token(ID, 'number'),
+            Token(INTEGER_DIV, 'DIV'),
+            Token(INTEGER_CONST, 4),
+            Token(SEMI, ';'),
+            Token(ID, 'c'),
+            Token(ASSIGN, ':='),
+            Token(ID, 'a'),
+            Token(MINUS, '-'),
+            Token(MINUS, '-'),
+            Token(ID, 'b'),
+            Token(END, 'END'),
+            Token(SEMI, ';'),
+            Token(ID, 'x'),
+            Token(ASSIGN, ':='),
+            Token(INTEGER_CONST, 11),
+            Token(SEMI, ';'),
+            Token(ID, 'y'),
+            Token(ASSIGN, ':='),
+            Token(INTEGER_CONST, 20),
+            Token(FLOAT_DIV, '/'),
+            Token(INTEGER_CONST, 7),
+            Token(PLUS, '+'),
+            Token(REAL_CONST, 3.14),
+            Token(SEMI, ';'),
+            Token(END, 'END'),
+            Token(DOT, '.')
         ]
         self.assertEqual(tokens, expected)
 
